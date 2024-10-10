ff4f8070fdd8db4ea3dc889efe86d47b3a263b10
Luis Leal <luisrrleal@gmail.com>
2024-10-09 21:09:06 -0500

    Ant material working

---
+++ app/_layout.tsx
@@ -0,0 +1,8 @@
+import { Slot } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function HomeLayout() {
+    return <SafeAreaView>
+        <Slot />
+    </SafeAreaView>;
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,21 @@
-import { Text } from "react-native"
+import { Input, Form, Button } from '@ant-design/react-native';
+import React from 'react';
 
 const HomePage = () => {
     return (
-        <Text>Hola</Text>
-    )
+        // <Input prefix="icon" placeholder="Basic Input" />
+        <Form>
+            <Form.Item label="Correo electrónico">
+                <Input type='text' placeholder="usuario@micorreo.com" />
+            </Form.Item>
+            <Form.Item label="Contraseña">
+                <Input type='password' placeholder="Contraseña" />
+            </Form.Item>
+            <Form.Item>
+                <Button type="primary">Registrarme</Button>
+            </Form.Item>
+        </Form>
+    );
 }
 
-export default HomePage
+export default HomePage;
