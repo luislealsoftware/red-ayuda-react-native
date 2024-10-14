f8042d1b5d45a14fa3005fbea65ea0619414dee9
Luis Leal <luisrrleal@gmail.com>
2024-10-14 11:44:33 -0500

    Add auth to login

--- app/auth/login/index.tsx
+++ app/auth/login/index.tsx
@@ -1,13 +1,48 @@
 import { Link } from 'expo-router';
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Div, Icon, Input, Text } from 'react-native-magnus';
+import { supabase } from '../../../lib/supabase';
+import { Alert } from 'react-native';
 
 
 const LoginScreen = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [loading, setLoading] = useState(false)
+
+    async function signInWithEmail() {
+        setLoading(true)
+        const { error } = await supabase.auth.signInWithPassword({
+            email: email,
+            password: password,
+        })
+
+        if (error) Alert.alert(error.message)
+        setLoading(false)
+    }
+
+    async function signUpWithEmail() {
+        setLoading(true)
+        const {
+            data: { session },
+            error,
+        } = await supabase.auth.signUp({
+            email: email,
+            password: password,
+        })
+
+        if (error) Alert.alert(error.message)
+        if (!session) Alert.alert('Please check your inbox for email verification!')
+        setLoading(false)
+    }
+
     return (
         <Div m="3xl" alignItems='center'>
             <Text fontSize="6xl">Iniciar sesión</Text>
             <Input
+                onChangeText={(text) => setEmail(text)}
+                value={email}
                 mt="md"
                 placeholder="Correo electrónico"
                 p={10}
@@ -16,6 +51,8 @@ const LoginScreen = () => {
             />
 
             <Input
+                onChangeText={(text) => setPassword(text)}
+                value={password}
                 placeholder="Contraseña"
                 mt="md"
                 p={10}
@@ -25,7 +62,13 @@ const LoginScreen = () => {
             />
 
             <Div>
-                <Button block mt="md" bg="blue700" color="white">
+                <Button
+                    block
+                    mt="md"
+                    bg="blue700"
+                    color="white"
+                    disabled={loading}
+                    onPress={() => signInWithEmail()}>
                     Iniciar sesión</Button>
             </Div>
 
